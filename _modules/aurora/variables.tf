e269b5c7286afe4b2d5c174c72caa4249d1a6c08
randygoz-dropstat <randy.goz@dropstat.com>
2026-06-05 02:13:03 -0400

    feat: ecs_security_group_ids as list for multiple ECS services

--- _modules/aurora/variables.tf
+++ _modules/aurora/variables.tf
@@ -1,5 +1,9 @@
 variable "name" { type = string }
-variable "ecs_security_group_id" { type = string }
+variable "ecs_security_group_ids" {
+  type        = list(string)
+  description = "List of ECS task security group IDs allowed to connect to Aurora"
+  default     = []
+}
 
 variable "database_name" {
   type    = string
