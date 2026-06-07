6f1b5b6574d9671ffcd5cb1f15b55175f543ea87
Randy Gonzalez Paz <rgonzalezp@transbank.cl>
2026-06-07 00:17:51 -0400

    fix(route53-zone): add missing variables.tf for native resource

---
+++ _modules/route53-zone/variables.tf
@@ -0,0 +1,31 @@
+variable "zone_name" {
+  type        = string
+  description = "DNS zone name, e.g. dev.dropstat-np.com"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to associate with the private zone"
+}
+
+variable "vpc_region" {
+  type        = string
+  description = "AWS region of the VPC"
+  default     = "us-east-2"
+}
+
+variable "comment" {
+  type    = string
+  default = ""
+}
+
+variable "force_destroy" {
+  type        = bool
+  default     = true
+  description = "Delete all records before destroying the zone. Needed when recreating (e.g. name change)."
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
